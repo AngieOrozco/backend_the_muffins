eeea1ba97d1022e4f622a4e84d21995f58a767bf
AngieOrozco <112782838+AngieOrozco@users.noreply.github.com>
2025-04-12 12:52:52 +0200

    changes

---
+++ myFirstApiRest/auctions/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers 
+from .models import Category, Auction 
+from django.utils import timezone 
+
+class CategoryListCreateSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = Category 
+        fields = ['id','name'] 
+class CategoryDetailSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = Category 
+        fields = '__all__' 
+
+class AuctionListCreateSerializer(serializers.ModelSerializer): 
+    creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", read_only=True) 
+
+    closing_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ") 
+     
+    isOpen = serializers.SerializerMethodField(read_only=True)
+
+    class Meta: 
+        model = Auction 
+        fields = '__all__' 
+
+    def get_isOpen(self, obj): 
+        return obj.closing_date > timezone.now()
+    
+    def validate_closing_date(self, value): 
+        if value <= timezone.now(): 
+            raise serializers.ValidationError("Closing date must be greater than now.") 
+        return value 
+
+class AuctionDetailSerializer(serializers.ModelSerializer): 
+
+    creation_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ", read_only=True) 
+
+    closing_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ") 
+
+    isOpen = serializers.SerializerMethodField(read_only=True) 
+
+    class Meta: 
+        model = Auction 
+        fields = '__all__'
+    
+    def get_isOpen(self, obj): 
+        return obj.closing_date > timezone.now() 
+
+    def validate_closing_date(self, value): 
+        if value <= timezone.now(): 
+            raise serializers.ValidationError("Closing date must be greater than now.") 
+        return value 
+
+

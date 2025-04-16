3a31b3f317cebc5c0cd48201d3761af74d3b3ace
202206694 <202206694@alu.comillas.edu>
2025-04-16 13:19:26 +0200

    render3

--- myFirstApiRest/myFirstApiRest/settings.py
+++ myFirstApiRest/myFirstApiRest/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta 
+import os 
+import dj_database_url 
+from dotenv import load_dotenv 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,12 +85,10 @@ WSGI_APPLICATION = "myFirstApiRest.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+load_dotenv() 
+DATABASES = { 
+'default': dj_database_url.config(default=os.getenv("DATABASE_URL")) 
+} 
 
 
 # Password validation
